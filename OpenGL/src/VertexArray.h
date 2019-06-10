5e0bea412abda6104b144af6b148e030a1d1020f
Bryan Ma <bma@gallagherdesign.com>
2019-06-09 21:11:09 -0400

    14

---
+++ OpenGL/src/VertexArray.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+
+class VertexArray
+{
+private:
+	unsigned int m_rendererID;
+public:
+	VertexArray();
+	~VertexArray();
+
+	void addBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout);
+
+	void bind() const;
+	void unbind() const;
+};
+
--- OpenGL/src/application.cpp
+++ OpenGL/src/application.cpp
@@ -9,6 +9,7 @@
 #include "Renderer.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
+#include "VertexArray.h"
 
 struct ShaderProgramSource
 {
@@ -130,10 +131,12 @@ int main(void)
 		GLCall(glGenVertexArrays(1, &vao));
 		GLCall(glBindVertexArray(vao));
 
+		VertexArray va;
 		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
 
-		GLCall(glEnableVertexAttribArray(0));
-		GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+		VertexBufferLayout layout;
+		layout.push<float>(2);
+		va.addBuffer(vb, layout);
 
 		IndexBuffer ib(indices, 6);
 
@@ -160,7 +163,8 @@ int main(void)
 
 			GLCall(glUseProgram(shader));
 			GLCall(glUniform4f(location, r, 0.3f, 1.0f, 1.0f));
-			GLCall(glBindVertexArray(vao));
+
+			va.bind();
 			ib.bind();
 
 			GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
