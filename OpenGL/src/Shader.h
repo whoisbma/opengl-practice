e5ec2eadf2bafc88336ce28de0cd59592ba5ad3d
Bryan Ma <bma@gallagherdesign.com>
2019-06-10 00:53:55 -0400

    17 - textures

--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -23,6 +23,8 @@ public:
 	void unbind() const;
 
 	// set uniforms
+	void setUniform1i(const std::string& name, int value);
+	void setUniform1f(const std::string& name, float value);
 	void setUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 private:
 	int getUniformLocation(const std::string& name);
--- OpenGL/src/application.cpp
+++ OpenGL/src/application.cpp
@@ -12,6 +12,7 @@
 #include "VertexArray.h"
 #include "VertexBufferLayout.h"
 #include "Shader.h"
+#include "Texture.h"
 
 int main(void)
 {
@@ -45,10 +46,10 @@ int main(void)
 
 	{
 		float positions[] = {
-			-0.5f, -0.5f,
-			 0.5f, -0.5f,
-			 0.5f,  0.5f,
-			-0.5f,  0.5f,
+			-0.5f, -0.5f, 0.0f, 0.0f,
+			 0.5f, -0.5f, 1.0f, 0.0f,
+			 0.5f,  0.5f, 1.0f, 1.0f,
+			-0.5f,  0.5f, 0.0f, 1.0f
 		};
 
 		unsigned int indices[] = {
@@ -56,18 +57,26 @@ int main(void)
 			2, 3, 0
 		};
 
+		GLCall(glEnable(GL_BLEND));
+		GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
 		VertexArray va;
-		VertexBuffer vb(positions, 4 * 2 * sizeof(float));
+		VertexBuffer vb(positions, 4 * 4 * sizeof(float)); // 4 floats per vertex
 
 		VertexBufferLayout layout;
 		layout.push<float>(2);
+		layout.push<float>(2);	//  add texture coords
 		va.addBuffer(vb, layout);
 
 		IndexBuffer ib(indices, 6);
 
 		Shader shader("res/shaders/Basic.shader");
 		shader.bind();
-		shader.setUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+		shader.setUniform4f("u_color", 0.8f, 0.3f, 0.8f, 1.0f);
+
+		Texture texture("res/textures/doge.jpg");
+		texture.bind();
+		shader.setUniform1i("u_texture", 0);
 
 		va.unbind();
 		vb.unbind();
@@ -83,7 +92,7 @@ int main(void)
 			renderer.clear();
 
 			shader.bind();
-			shader.setUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+			shader.setUniform4f("u_color", r, 0.3f, 0.8f, 1.0f);
 
 			renderer.draw(va, ib, shader);
 
